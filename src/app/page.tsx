1da42b02a23cd8b6bc101fef43a78f6363985d2b
DaviMattosDev <daviescolarj@gmail.com>
2024-05-05 00:50:30 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,168 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
-export default function Home() {
+interface Opiniao {
+  nome: string;
+  email: string;
+  opiniao: string;
+}
+
+export default function Opiniao() {
+  const [opinioes, setOpinioes] = useState<Opiniao[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [opiniao, setOpiniao] = useState<string>("");
+  const [nome, setNome] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+
+  // Opiniões estáticas como exemplo
+  const opinioesEstaticas: Opiniao[] = [
+    {
+      nome: "João",
+      email: "joao@example.com",
+      opiniao: "Podiam melhorar serviço, estou parcialmente satisfeito!",
+    },
+    {
+      nome: "Maria",
+      email: "maria@example.com",
+      opiniao: "Ótimos produtos entretando o atendimento está péssimo.",
+    },
+    {
+      nome: "Pedro",
+      email: "pedro@example.com",
+      opiniao: "Podem melhorar mais!!!",
+    },
+  ];
+
+  useEffect(() => {
+    setOpinioes(opinioesEstaticas);
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === opinioes.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000); // Troca de opinião a cada 5 segundos
+
+    return () => clearInterval(intervalId);
+  }, [opinioes]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (opiniao.trim() === "" || nome.trim() === "" || email.trim() === "") {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+    const novaOpiniao = {
+      nome: nome,
+      email: email,
+      opiniao: opiniao,
+    };
+    setOpinioes([...opinioes, novaOpiniao]);
+    setOpiniao("");
+    setNome("");
+    setEmail("");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-semibold mb-4">
+        Deixe seu feedback construtivo:
+      </h1>
+      <div className="col-span-3">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white rounded-lg shadow-lg p-6"
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="nome"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Seu Nome:
+            </label>
+            <input
+              type="text"
+              id="nome"
+              className="w-full rounded border-gray-300 focus:border-blue-500 focus:ring-blue-500 text-black p-2"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
             />
-          </a>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Seu Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full rounded border-gray-300 focus:border-blue-500 focus:ring-blue-500 text-black p-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="opiniao"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Sua Opinião:
+            </label>
+            <textarea
+              id="opiniao"
+              className="w-full rounded border-gray-300 focus:border-blue-500 focus:ring-blue-500 text-black p-2"
+              rows={4}
+              value={opiniao}
+              onChange={(e) => setOpiniao(e.target.value)}
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          >
+            Enviar Opinião
+          </button>
+        </form>
+
+        <h2 className="text-2xl font-semibold mb-4 p-4">
+          Opinião dos últimos clientes:
+        </h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {opinioes.map((opiniao, index) => (
+            <div
+              key={index}
+              className={`bg-white rounded-lg shadow-lg p-6 transition-opacity duration-500 ${
+                index === currentIndex ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <div className="flex items-center mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-12 h-12 mr-4 text-gray-500"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <div>
+                  <h2 className="text-lg font-semibold">{opiniao.nome}</h2>
+                  <p className="text-gray-500">{opiniao.email}</p>
+                </div>
+              </div>
+              <p className="text-gray-700">{opiniao.opiniao}</p>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
